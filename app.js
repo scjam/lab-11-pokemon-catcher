ef857d75b47bec4f7181b4f177d087bd410a6253
Sydney James <sydneyjames7@gmail.com>
2020-10-12 15:59:19 -0700

    add pokemon data and grab DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // import functions and grab DOM elements
+import { rawPokemonData } from './pokemon.js';
 
+const radios = document.querySelectorAll('input');
+const next = document.querySelector('button');
+const capture = document.querySelector('#capture');
+const images = document.querySelectorAll('label > img');
 // initialize state
 
 // set event listeners to update state and DOM
