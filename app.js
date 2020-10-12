f42c192a49e5712f979a809b57c5e25e425ac8c1
Sydney James <sydneyjames7@gmail.com>
2020-10-12 16:31:35 -0700

    random pokemon function working

--- app.js
+++ app.js
@@ -5,6 +5,38 @@ const radios = document.querySelectorAll('input');
 const next = document.querySelector('button');
 const capture = document.querySelector('#capture');
 const images = document.querySelectorAll('label > img');
+
 // initialize state
+let pokemonCaptured = 0;
+
+function getRandomPokemon(someArray) {
+    const index = Math.floor(Math.random() * someArray.length);
+
+    return someArray[index];
+
+}
+
+let pokemonOne = getRandomPokemon(rawPokemonData); 
+let pokemonTwo = getRandomPokemon(rawPokemonData); 
+let pokemonThree = getRandomPokemon(rawPokemonData); 
+
+while (pokemonOne.id === pokemonTwo.id) {
+    pokemonOne = getRandomPokemon(rawPokemonData);
+}
+while (pokemonTwo.id === pokemonThree.id) {
+    pokemonTwo = getRandomPokemon(rawPokemonData);
+}
+while (pokemonThree.id === pokemonOne.id) {
+    pokemonThree = getRandomPokemon(rawPokemonData);
+}
+
+radios[0].value = pokemonOne.id;
+images[0].src = pokemonOne.url_image;
+
+radios[1].value = pokemonTwo.id;
+images[1].src = pokemonTwo.url_image;
+
+radios[2].value = pokemonThree.id;
+images[2].src = pokemonThree.url_image;
 
 // set event listeners to update state and DOM
