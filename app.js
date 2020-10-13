48f4fa95d4fbe95c505abae093b0448bccc2de9b
Sydney James <sydneyjames7@gmail.com>
2020-10-13 15:44:35 -0700

    tracking captured and encountered working, total rounds set to 10. redirecting to empty results page

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 // import functions and grab DOM elements
 import { pokemonData } from './pokemon.js';
-import { findById, getFromLocalStorage, setInLocalStorage } from './utils.js';
+import { findById } from './utils.js';
 
 const radios = document.querySelectorAll('input');
-const next = document.querySelector('button');
-const captured = document.querySelector(':checked');
 const images = document.querySelectorAll('label > img');
 
 // initialize state
-let pokemonCaptured = 0;
-const pokemonEncountered = [];
-
-
+let totalRounds = 0;
+const resultsArray = [];
 
 function getRandomPokemon(someArray) {
     const index = Math.floor(Math.random() * someArray.length);
@@ -47,55 +43,82 @@ function randomizePokemon() {
     radios[2].value = pokemonThree.id;
     images[2].src = pokemonThree.url_image;
 
+    let pokemon = findById(resultsArray, Number(pokemonOne.id));
+
+    if (!pokemon) {
+        const newPokemon = findById(pokemonData, Number(pokemonOne.id));
+        pokemon = {
+            id: Number(pokemonOne.id),
+            name: newPokemon.pokemon,
+            encountered: 1,
+            captured: 0,
+        };
+        resultsArray.push(pokemon);
+    } else {
+        pokemon.encountered++;
+    }
+
+    let pokemon2 = findById(resultsArray, Number(pokemonTwo.id));
+
+    if (!pokemon2) {
+        const newPokemon = findById(pokemonData, Number(pokemonTwo.id));
+        pokemon2 = {
+            id: Number(pokemonTwo.id),
+            name: newPokemon.pokemon,
+            encountered: 1,
+            captured: 0,
+        };
+        resultsArray.push(pokemon2);
+    } else {
+        pokemon2.encountered++;
+    }
+
+    let pokemon3 = findById(resultsArray, Number(pokemonThree.id));
+
+    if (!pokemon3) {
+        const newPokemon = findById(pokemonData, Number(pokemonThree.id));
+        pokemon3 = {
+            id: Number(pokemonThree.id),
+            name: newPokemon.pokemon,
+            encountered: 1,
+            captured: 0,
+        };
+        resultsArray.push(pokemon3);
+    } else {
+        pokemon3.encountered++;
+    }
+
 }
 
 randomizePokemon();
 
-console.log(pokemonEncountered);
 //addEventListener(randomizePokemon);
 // set event listeners to update state and DOM
-const resultsArray = [];
+
 
 for (let i = 0; i < radios.length; i++) {
     radios[i].addEventListener('change', (e) => {
-        
-        radios.forEach ((radio) => {
-            let pokemon = findById(resultsArray, Number(radio.value));
-            console.log(radio.value);
 
-            if (!pokemon) {
-                const newPokemon = findById(pokemonData, Number(radio.value));
-                pokemon = {
-                    id: Number(radio.value),
-                    name: newPokemon.pokemon,
-                    encountered: 1,
-                    captured: 0,
-                };
-                resultsArray.push(pokemon);
-            } else {
-                pokemon.encountered++;
-            }
-        });
+        const pokemon = findById(resultsArray, Number(e.target.value));
+        pokemon.captured++;
+        totalRounds++;
+        
 
         for (let i = 0; i < radios.length; i++) {
             images[i].style.opacity = .5;
         }
-        pokemonCaptured++;
+
         randomizePokemon();
+        if (totalRounds === 10) {
+            window.location.href = './results.html';
+        }
         // const playerCapturedPokemon = e.target.value === pokemonCaptured.id;
         // const playerEncounteredPokemon = e.target.value === pokemonEncountered.id;
         
         
 
-        console.log(pokemonCaptured);
+        console.log(resultsArray);
 
-        //go into pokemon array and increment times captured i.e. shopping cart pokemon. e.target.value is used to decide which pokemon to mutate
 
     });
 }
-
-// const captured = document.querySelector(':checked');
-//         captured.value = ''
-// const pokemonInCart = findById(theIdOfThePokemonTheyCaptured, pokemonShoppingCart);
-        // pokemonInCart.captured++;
-        // savePokemon(pokemonShoppingCart)
--- utils.js
+++ utils.js
@@ -1,7 +1,6 @@
 export function findById(someArray, someId) {
     for (let i = 0; i < someArray.length; i++) {
         const item = someArray[i];
-        console.log(item);
         if (item.id === someId) {
 
             return item;
